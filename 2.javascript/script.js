1587ee14454671d8b8abbb657b07c99e759744d4
songi <10thddl04@gmail.com>
2024-08-12 15:20:47 +0900

    console 공부

---
+++ 2.javascript/script.js
@@ -0,0 +1,19 @@
+console.log('안녕하세요');
+console.log(123);
+const obj = {
+    a: 'a',
+    b:'b'
+}
+console.log(obj);
+
+console.table(obj);
+console.error('에러지롱 에러지롱');
+console.warn('경고지롱 경고지롱');
+console.time('Hello');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd('Hello');
+console.clear();
