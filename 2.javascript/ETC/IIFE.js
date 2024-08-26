d0e51d49ad9a2467122a7bff309dc21ef54414e9
songi <10thddl04@gmail.com>
2024-08-26 15:21:32 +0900

    IIFE

--- 2.javascript/ETC/IIFE.js
+++ 2.javascript/ETC/IIFE.js
@@ -1,6 +1,31 @@
-(function() {
-    let aName = 'Barry';
-    console.log(aName);
-}
-)();
-console.log(aName);
+const score = (() => {
+    let count = 0;
+    return {
+        current: () => {return count},
+        increment: () => {count ++ },
+        reset: () => {count = 0}
+    }
+})()
+
+console.log(typeof score);
+console.log(score);
+
+console.log(score.current());
+score.increment();
+console.log(score.current());
+
+console.log('------ increment ------')
+const increment = (() => {
+    let counter = 0;
+    console.log(counter);
+    const number = (num) => 
+        console.log(`it is ${num} number`);
+    return () => {
+        counter ++;
+        number(counter);
+    }
+})()
+
+increment();
+console.log(increment);
+increment();
