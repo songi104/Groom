69eb8b26812f7fc8d6cf7bac62cc0a38ef705057
songi <10thddl04@gmail.com>
2024-10-22 00:30:42 +0900

    initial commit

---
+++ java/src/sec03/Chap1/Ex1.java
@@ -0,0 +1,16 @@
+package sec03.Chap1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        byte _1b_byte = 1;
+        short _2b_short = 2;
+        int _4b_int = 3; // ⭐️ 일반적으로 널리 사용
+        long _8b_long = 4;
+
+        //  큰 자료형에 작은 자료형의 값을 넣을 수 있음
+        //  💡 묵시적(암시적) 형변환
+        _2b_short = _1b_byte;
+
+
+    }
+}
