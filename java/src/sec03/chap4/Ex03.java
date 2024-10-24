3ee034e7967fb29b1a9c3cc04d666da5668f0cf4
songi <10thddl04@gmail.com>
2024-10-24 23:24:49 +0900

    sec 03

---
+++ java/src/sec03/chap4/Ex03.java
@@ -0,0 +1,11 @@
+package sec03.chap4;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 0, d = 0, e=0, f= 0;
+        boolean bool1 = a<b && c++ < (d+=3);
+        boolean bool2 = a<b || e ++ < (f+=3);
+        boolean bool3 = a > b && c++ <(d+=3); // 변하지 않음!!
+        boolean bool4 = a > b || e++ < (f += 3);
+    }
+}
